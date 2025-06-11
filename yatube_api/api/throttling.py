7729485ed0f925d3eb4c67cf8d06801c0582d119
Maya010505 <mayushka0105@yandex.ru>
2025-06-11 13:04:41 +0300

    commit

--- yatube_api/api/throttling.py
+++ yatube_api/api/throttling.py
@@ -7,6 +7,6 @@ class WorkingHoursRateThrottle(throttling.BaseThrottle):
 
     def allow_request(self, request, view):
         now = datetime.datetime.now().hour
-        if 5 < now >= 3:
+        if 5 > now >= 3:
             return False
         return True
